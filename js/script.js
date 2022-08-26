25affa1232120b3854ce8ab59c76c9bf01f9aba4
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-26 17:22:16 +0200

    create layout HTML

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 console.log("Venerdì");
 
-let numRandomYou = Math.floor(Math.random() * 6) + 1;
-let numRandomCpu = Math.floor(Math.random() * 6) + 1;
+let buttonPlay = document.getElementById("play");
 
-console.log(numRandomYou, numRandomCpu);
+buttonPlay.addEventListener("click", function () {
+  let numRandomYou = Math.floor(Math.random() * 6) + 1;
+  let numRandomCpu = Math.floor(Math.random() * 6) + 1;
+  let punteggioYou = document.getElementById("punteggioYou");
+  let punteggioCpu = document.getElementById("punteggioCpu");
+  let result = document.getElementById("result");
+  let newgame = document.getElementById("newgame");
+  console.log(numRandomYou, numRandomCpu);
 
-if (numRandomYou > numRandomCpu) {
-  console.log("Hai vinto!");
-} else if (numRandomYou === numRandomCpu) {
-  console.log("Pareggio");
-} else {
-  console.log("Hai perso.. Ritenta!");
-}
+  punteggioYou.innerHTML = numRandomYou;
+  punteggioCpu.innerHTML = numRandomCpu;
+  newgame.style.visibility = "visible";
+
+  newgame.addEventListener("click", function () {
+    location.reload();
+  });
+
+  if (numRandomYou > numRandomCpu) {
+    result.innerHTML = "Hai vinto!";
+  } else if (numRandomYou === numRandomCpu) {
+    result.innerHTML = "Pareggio";
+  } else {
+    result.innerHTML = "Hai perso... Ritenta!";
+  }
+});
