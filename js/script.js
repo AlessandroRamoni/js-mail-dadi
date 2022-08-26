84e3a0571741810bc025bb53343ff3f8f6fca222
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-26 14:52:03 +0200

    create if/else

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ let numRandomYou = Math.floor(Math.random() * 6) + 1;
 let numRandomCpu = Math.floor(Math.random() * 6) + 1;
 
 console.log(numRandomYou, numRandomCpu);
+
+if (numRandomYou > numRandomCpu) {
+  console.log("Hai vinto!");
+} else if (numRandomYou === numRandomCpu) {
+  console.log("Pareggio");
+} else {
+  console.log("Hai perso.. Ritenta!");
+}
