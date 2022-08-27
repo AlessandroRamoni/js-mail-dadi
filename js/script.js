e5bc87ad95ce432e2ed757e0d8474a7546576f15
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-08-27 15:54:34 +0200

    create button reload

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ buttonPlay.addEventListener("click", function () {
   let punteggioCpu = document.getElementById("punteggioCpu");
   let result = document.getElementById("result");
   let newgame = document.getElementById("newgame");
+  let reload = document.getElementById("reload");
   console.log(numRandomYou, numRandomCpu);
   const quadratosx = document.getElementById("quadratosx");
   const quadratodx = document.getElementById("quadratodx");
@@ -17,6 +18,7 @@ buttonPlay.addEventListener("click", function () {
   punteggioYou.innerHTML = numRandomYou;
   punteggioCpu.innerHTML = numRandomCpu;
   newgame.style.visibility = "visible";
+  // reload.style.visibility = "visible";
   buttonPlay.style.display = "none";
   // buttonPlay.style.visibility = "hidden";
 
@@ -24,9 +26,11 @@ buttonPlay.addEventListener("click", function () {
 
   quadratosx.style.transform = `rotate(${numeroRandom30(-30, 30)}deg)`;
   quadratodx.style.transform = `rotate(${numeroRandom30(-30, 30)}deg)`;
+  quadratosx.style.borderRadius = "30px";
+  quadratodx.style.borderRadius = "30px";
 
   /*
-ho provato ad utilizzare querySelectorAll al posto dei due ID (quadrato sx e quadratodx) ma esce un allarme in console... why?!
+ho provato ad utilizzare querySelectorAll al posto dei due ID (quadratosx e quadratodx) ma esce un allarme in console... why?!
 */
 
   newgame.addEventListener("click", function () {
@@ -35,10 +39,16 @@ ho provato ad utilizzare querySelectorAll al posto dei due ID (quadrato sx e qua
 
   if (numRandomYou > numRandomCpu) {
     result.innerHTML = "Hai vinto!";
+    quadratosx.style.border = "8px solid green";
+    quadratodx.style.border = "3px solid red";
   } else if (numRandomYou === numRandomCpu) {
     result.innerHTML = "Pareggio";
+    quadratosx.style.border = "5px solid yellow";
+    quadratodx.style.border = "5px solid yellow";
   } else {
     result.innerHTML = "Hai perso... Ritenta!";
+    quadratosx.style.border = "3px solid red";
+    quadratodx.style.border = "8px solid green";
   }
 });
 
